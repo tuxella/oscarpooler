e1479c509c465a98f29e5912ad820540d77bbef1
Thomas CORDIVAL <github@lukita.fr>
2011-09-19 08:48:14 +0200

    Cleanup in the useless functions in views.py

--- journey/views.py
+++ journey/views.py
@@ -20,34 +20,6 @@ def journey_detail_from_url (request, journey_url):
         return render_to_response('journey/error.html',
                                   {'debug': "This journey doesn't exist"})
 
-
-def journey_detail(request, journey_id):
-    journey = Journey.objects.get(pk=journey_id)
-    av = journey.available_seats()
-    sys.stderr.write("views.py")
-    peoples_json = "{}"
-    peoples = People.objects.filter(journey = journey_id)
-    if 0 < len(peoples):
-        js = json_object(peoples[0])
-        sys.stderr.write("#" * 50)
-        sys.stderr.write("obj : %s" % js)
-        peoples_json = "[" + ",".join(map(json_object, peoples)) + "]"
-
-    vehicles_json = "{}"
-    vehicles = Vehicle.objects.filter(journey = journey_id)
-    if 0 < len(vehicles):
-        js = json_object(vehicles[0])
-        sys.stderr.write("#" * 50)
-        sys.stderr.write("obj : %s" % js)
-        vehicles_json = "[" + ",".join(map(json_object, vehicles)) + "]"
-
-    return render_to_response('journey/detail.html',
-                              {'journey': journey,
-                               'seats':av,
-                               'peoples':peoples_json,
-                               'vehicles':vehicles_json})
-
-
 def journey_edit(request, journey_id):
     journey = Journey.objects.get(pk=journey_id)
     vehicles = Vehicle.objects.filter(journey = journey_id)
@@ -56,7 +28,6 @@ def journey_edit(request, journey_id):
     return render_to_response('journey/edit.html',
                               {'journey': journey,
                                'seats':av})
-
 
 def journey_form_render(request, journey_id):
     c = {}
@@ -123,6 +94,7 @@ def journey_form(request, journey_id):
             return journey_form_render(request, journey_id)
     else:
         return journey_form_render(request, journey_id)
+
 def current_site_url():
     url = getattr(settings, 'MY_DJANGO_URL_PATH', '')
     return url
@@ -174,103 +146,3 @@ def index(request):
     for j in journeys:
         journeys_and_seats.append((j,j.available_seats()))
     return render_to_response('journey/index.html', {'journeys_and_seats': journeys_and_seats})
-
-def json_object(obj = None):
-    if obj is None:
-        return ("{}")
-    json_obj = {}
-    attrs = obj._meta.fields
-    for attr in obj._meta.fields:
-        attr_value = obj.__getattribute__(attr.name)
-        str_value = ""
-#        sys.stderr.write("[%s] = %s (%s)\n" % (attr.name, attr_value, type(attr_value)))
-        if "id" in dir(attr_value):
-            str_value = attr_value.id
-        elif datetime == type(attr_value):
-            sys.stderr.write("[%s] = %s (%s)\n" % (attr.name, attr_value, dir(attr_value)))
-            str_value = "%.2d:%.2d %.4d/%.2d/%.2d" % (attr_value.hour,
-                                                      attr_value.minute,
-                                                      attr_value.year,
-                                                      attr_value.month,
-                                                      attr_value.day)
-        else:
-            str_value = unicode(attr_value)
-        json_obj[attr.name] = str_value
-
-    return json.dumps(json_obj)
-
-def people_new(request):
-    try:
-        if "POST" != request.method:
-            return render_to_response('journey/error.html',
-                                      {'debug': "Only POST method is allowed on this URL"})
-        request_items = request.POST.items()
-        if (1 != len(request_items)):
-            return render_to_response('journey/error.html',
-                                      {'message': "Incorrect number of params (1 expected)",
-                                       'debug': "[%s]" % request.POST})
-        sys.stderr.write("Data raw: %s\n" % request_items)
-        sys.stderr.write("Data len: %s\n" % len(request_items))
-        json_str = str(request_items[0][0])
-        json_data = json.loads(json_str)
-        name = json_data["name"]
-        if "count" in json_data:
-            count = json_data["count"]
-        else:
-            count = 1
-        p = People(name = name, count = count)
-        p.save()
-        return HttpResponse(json_object(p))
-
-    except Exception as e:
-        sys.stderr.write("ERROR:\n")
-        sys.stderr.write("%s" % type(e))
-        sys.stderr.write("%s" % e.message)
-        return render_to_response('journey/error.html',
-                                  {'debug': "[%s]" % request.POST})
-
-def vehicle_new(request):
-    try:
-        if "POST" != request.method:
-            return render_to_response('journey/error.html',
-                                      {'debug': "Only POST method is allowed on this URL"})
-        request_items = request.POST.items()
-        if (1 != len(request_items)):
-            return render_to_response('journey/error.html',
-                                      {'message': "Incorrect number of params (1 expected)",
-                                       'debug': "[%s]" % request.POST})
-        sys.stderr.write("Data raw: %s\n" % request_items)
-        sys.stderr.write("Data len: %s\n" % len(request_items))
-        json_str = str(request_items[0][0])
-        json_data = json.loads(json_str)
-        name = json_data["name"]
-        p = People(name = name, count = count)
-        p.save()
-        return HttpResponse(json_object(p))
-
-    except Exception as e:
-        sys.stderr.write("ERROR:\n")
-        sys.stderr.write("%s" % type(e))
-        sys.stderr.write("%s" % e.message)
-        return render_to_response('journey/error.html',
-                                  {'debug': "[%s]" % request.POST})
-
-def journey_json(request, journey_id):
-    journey = Journey.objects.get(pk=journey_id)
-    return HttpResponse(json_object(journey))
-
-def people_json(request, people_id):
-    people = People.objects.get(pk=people_id)
-    return HttpResponse(json_object(people))
-
-def vehicle_json(request, vehicle_id):
-    vehicle = Vehicle.objects.get(pk=vehicle_id)
-    return HttpResponse(json_object(vehicle))
-
-def ootest(request):
-    sys.stderr.write("In test\n")
-    c = {}
-    c.update(csrf(request))
-    sys.stderr.write("%s\n" % c)
-    return render_to_response('journey/debug.html',
-                              c) #{'debug': "Only POST method is allowed on this URL"})
