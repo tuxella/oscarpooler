3003de6f1582813b2e191a113fdb615b152f7397
Thomas CORDIVAL <github@lukita.fr>
2011-09-19 08:50:06 +0200

    Clean up in the urls

--- urls.py
+++ urls.py
@@ -7,16 +7,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        (r'^admin/', include(admin.site.urls)),
-                       (r'^journeys/(?P<journey_id>\d+)/$', 'journey.views.journey_detail'),
                        (r'^journeys/(?P<journey_id>\d+)/edit$', 'journey.views.journey_form'),
                        (r'^journeys/$', 'journey.views.index'),
                        (r'^journeys/new$', 'journey.views.journey_new'),
                        (r'^journeys/new/(?P<journey_id>\d+)/$', 'journey.views.journey_view'),
-                       (r'^j/(?P<journey_url>\.+)/$', 'journey.views.journey_detail_from_url'),
-                       (r'^peoples/new$', 'journey.views.people_new'),
-                       (r'^peoples/json/(?P<people_id>\d+)/$', 'journey.views.people_json'),
-                       (r'^vehicles/json/(?P<vehicle_id>\d+)/$', 'journey.views.vehicle_json'),
-                       (r'^test$', 'journey.views.ootest'),
                        (r'^media/(?P<path>.*)$', 'django.views.static.serve'),
                        (r'', 'journey.views.index')
 )
